c2dec089980fa244ee451c6f47ce7c2dd3230308
Theo92100e <tniemann92@gmail.com>
2025-03-18 09:38:03 +0100

    pushed correction

--- tasks/inpainting.py
+++ tasks/inpainting.py
@@ -49,7 +49,7 @@ class Inpainting(LinearOperator):
         # 1 / (mask/sigma^2 + 1/rho^2)
         #do the same but with sherman morrison to see
         quantity= rho**2/(sigma**2+rho**2) 
-        inv_var = rho**2(1-2*quantity*self.mask)
+        inv_var = rho**2(torch.eye(self.mask.shape)-2*quantity*self.mask)
         #inv_var = 1 / (2*self.mask / (sigma**2) + 1 / (rho**2))
         noise = torch.sqrt(inv_var) * torch.randn_like(x)
         mu_x = inv_var * (self.mask * y / (sigma**2) + x / (rho**2))
